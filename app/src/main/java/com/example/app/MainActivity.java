c1aae312462eda2c8143d49abbcc5629f8adc9d2
Reezlaw <51858050+Reezlaw@users.noreply.github.com>
2025-12-05 01:03:23 +0100

    Change web view URL from pstream to sflix

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-        mWebView.loadUrl("https://pstream.mov");
+        mWebView.loadUrl("https://sflix.is/home");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
